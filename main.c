fcf8dd759f8d3fbd80d15fb2deca1232d75f56ac
Andrea Berardi <Kotheos@protonmail.com>
2021-11-06 01:11:21 +0100

    refactored code and added some templates to prepare it for new experiments

--- main.c
+++ main.c
@@ -22,20 +22,21 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    srand(SEED); // fixed seed in order to get reproducible results
-
     // Experiment n.1 - Insertion Sort
+    srand(SEED); // Reset seed - a fixed seed ensures reproducible results
     lab_1(fp, 0, 50000, 10, 1000, DEBUG_MODE);
+
+    // Experiment n.2 - Merge Sort and Hybrid Sort
+    srand(SEED); // Reset seed - a fixed seed ensures reproducible results
+
+    // Experiment n.3 - TODO
+    srand(SEED); // Reset seed - a fixed seed ensures reproducible results
+
+    // Experiment n.4 - TODO
+    srand(SEED); // Reset seed - a fixed seed ensures reproducible results
 
     if (fclose(fp) == EOF)
         fprintf(stderr, "Failed to flush buffered data on `%s`\n", argv[1]);
-
-    int A[] = {1, 3, 2, 0, 5, 6, 9, 11, 113, 112, 66, 77, 2, 11, 11, 11, 11, 8, 7, 54, 45};
-    print_array(A, 21);
-    is_sorted(A, 21) ? printf("sorted\n") : printf("not sorted\n");
-    MergeSort(A, 0, 21 - 1);
-    print_array(A, 21);
-    is_sorted(A, 21) ? printf("sorted\n") : printf("not sorted\n");
 
     return EXIT_SUCCESS;
 }
