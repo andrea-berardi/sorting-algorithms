ea2b5fe8cbc6470667ddd9d731ea39ad750154a4
Andrea Berardi <Kotheos@protonmail.com>
2021-11-06 14:53:26 +0100

    refactored code

--- src/modules/sortingAlgorithms.c
+++ src/modules/sortingAlgorithms.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../../headers/utils.h"
-#include "../../headers/sortingAlgorithms.h"
+#include "../headers/utils.h"
+#include "../headers/sortingAlgorithms.h"
 
 // Insertion sort
 void InsertionSort(int array[], size_t length) {
