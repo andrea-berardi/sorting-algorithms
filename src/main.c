06de47cd40360e8465736ce3a73d0418927f1c7b
Andrea Berardi <Kotheos@protonmail.com>
2021-11-09 01:20:27 +0100

    experiment 2B (different from "2")

--- src/main.c
+++ src/main.c
@@ -10,7 +10,6 @@ const size_t THRESHOLD = 439;
 const unsigned SEED = 10;
 
 int main(int argc, char *argv[]) {
-    /*
     if (argc != 2) {
         fprintf(stderr, "Not enough arguments. Usage: `./%s <file>`\n", argv[0]);
         exit(EXIT_FAILURE);
@@ -21,17 +20,16 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Failed to open file `%s`\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-     */
 
     // Experiment n.1 - Insertion Sort
-    //lab_1("../results/lab_1.csv", 0, 50000, 50, 1000, SEED, DEBUG_MODE);
+    lab_1("../results/lab_1.csv", 0, 50000, 50, 1000, SEED, DEBUG_MODE);
 
     // Experiment n.2A - Merge Sort
-    //lab_2A("../results/lab_2A.csv", 0, 50000, 50, 1000, SEED, DEBUG_MODE);
+    lab_2A("../results/lab_2A.csv", 0, 50000, 50, 1000, SEED, DEBUG_MODE);
 
     // Experiment n.2B - Find T (threshold) for Merge Sort and Insertion Sort
-    //lab_1("../results/lab_2B-1.csv", 0, 5000, 100, 1, SEED, DEBUG_MODE); // finding the threshold
-    //lab_2A("../results/lab_2B-2.csv", 0, 5000, 100, 1, SEED, DEBUG_MODE); // finding the threshold
+    lab_1("../results/lab_2B-1.csv", 0, 5000, 100, 1, SEED, DEBUG_MODE); // finding the threshold
+    lab_2A("../results/lab_2B-2.csv", 0, 5000, 100, 1, SEED, DEBUG_MODE); // finding the threshold
     lab_2B("../results/lab_2B.csv", 0, 50000, 50, 1000, THRESHOLD, SEED, DEBUG_MODE);
 
     // Experiment n.2 - Insertion Sort, Merge Sort, Hybrid Sort (TODO)
@@ -40,10 +38,8 @@ int main(int argc, char *argv[]) {
 
     // Experiment n.4 - (TODO)
 
-    /*
     if (fclose(fp) == EOF)
         fprintf(stderr, "Failed to flush buffered data on `%s`\n", argv[1]);
-    */
 
     return EXIT_SUCCESS;
 }
